d58237a2d7f9082f46996073b381475eab0852a0
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-20 23:45:27 +0330

    improve code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="w-full h-screen">
+    <div className="w-full max-w-[1000px] mx-auto bg-green-200 mt-4 py-5 px-32">
+hjhdjs
+    </div>
+  </div>;
 }
 
 export default App;
