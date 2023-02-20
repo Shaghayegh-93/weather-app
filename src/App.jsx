32628c17664dac2069bc52cdfa7e425f8186bfd2
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-20 23:57:58 +0330

    import and render CityList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import CityList from "./components/CityList";
 
 function App() {
-  return <div className="w-full h-screen">
-    <div className="w-full max-w-[1000px] mx-auto bg-green-200 mt-4 py-5 px-32">
-hjhdjs
+  return (
+    <div className="w-full h-screen">
+      <div className="w-full max-w-[1000px] mx-auto bg-green-200 mt-4 py-5 px-32">
+        <CityList />
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
