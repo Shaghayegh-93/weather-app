933e1e01c200b7136fc3c56412e513504e7c47c4
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-21 23:33:11 +0330

    import and render InputCity

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import CityList from "./components/CityList";
+import InputCity from "./components/InputCity";
 
 function App() {
   return (
     <div className="w-full h-screen">
       <div className="w-full max-w-[1000px] mx-auto bg-green-200 mt-4 py-5 px-32">
         <CityList />
+        <InputCity/>
       </div>
     </div>
   );
