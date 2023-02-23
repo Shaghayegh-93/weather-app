f57d67f9711ed273f076f74eb9ac0ce8a0ea8ab0
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-23 23:21:18 +0330

    improve style

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import InputCity from "./components/InputCity";
 
 function App() {
   return (
-    <div className="w-full h-screen">
-      <div className="w-full max-w-[1000px] mx-auto bg-green-200 mt-4 py-5 px-32">
+    <div className="w-full h-screen px-4">
+      <div className=" max-w-[1000px] w-full mx-auto bg-green-200 mt-4 py-5 px-32">
         <CityList />
         <InputCity/>
       </div>
