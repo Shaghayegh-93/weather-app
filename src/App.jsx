0e44ce4881340f1d315c1e5dd66c777941e62983
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-26 23:24:25 +0330

    import and render TimeAndLocation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import CityList from "./components/CityList";
 import InputCity from "./components/InputCity";
+import TimeAndLocation from "./components/TimeAndLocation";
 
 function App() {
   return (
     <div className="w-full h-screen px-4">
-      <div className=" max-w-[1000px] w-full mx-auto bg-green-200 mt-4 py-5 px-32">
+      <div className=" max-w-[1000px] w-full mx-auto bg-green-200 mt-4 py-5 sm:px-32 px-4">
         <CityList />
-        <InputCity/>
+        <InputCity />
+        <TimeAndLocation />
       </div>
     </div>
   );
