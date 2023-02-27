c37f469a8e8b94ed0ac9dcd37c755fe5ca33e614
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-27 23:56:23 +0330

    import and render WeatherForecastDetail and WeatherForecast coponent

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import CityList from "./components/CityList";
 import DetailWeatherInfo from "./components/DetailWeatherInfo";
 import InputCity from "./components/InputCity";
 import TimeAndLocation from "./components/TimeAndLocation";
+import WeatherForecast from "./components/WeatherForecast";
 
 function App() {
   return (
-    <div className="w-full h-screen px-4">
-      <div className=" max-w-[1000px] w-full mx-auto bg-green-200 mt-4 py-5 sm:px-32 px-4">
+    
+      <div className=" max-w-screen-md h-fit mx-auto bg-green-200 my-4 py-4 sm:px-32 px-4 shadow-xl ">
         <CityList />
         <InputCity />
         <TimeAndLocation />
-        <DetailWeatherInfo/>
+        <DetailWeatherInfo />
+        <WeatherForecast title="HOURLY FORECAT" />
+        <WeatherForecast title="DAILY FORECAT" />
       </div>
-    </div>
+  
   );
 }
 
