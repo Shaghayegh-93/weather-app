334cdef053f48dea8924d7b82ad1655515691649
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-28 05:33:43 +0330

    improve design

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const TimeAndLocation = () => {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between">
-      <div className="my-3 ">
-        <p className="font-extralight sm:text-lg">
+      <div className="my-2 ">
+        <p className="font-bold sm:text-lg">
           Tuseday,31 May 2022 |Local time:12:17 PM
         </p>
       </div>
       <div className="sm:font-extralight sm:text-lg font-bold">
-        <p>Berlin,DE</p>
+        <p className="font-bold">Berlin,DE</p>
       </div>
     </div>
   );
