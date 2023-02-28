597304e2881bd53f09bd2af5f48360a7a961335c
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-02-28 05:34:38 +0330

    improve design

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -8,7 +8,7 @@ const CityList = () => {
     { id: Math.floor(Math.random() * 1000), title: "manchester" },
   ];
   return (
-    <div className=" mb-8">
+    <div className=" mb-2">
       <ul className="hidden  sm:flex items-center justify-between ">
         {cities.map((city) => (
           <li key={city.id} className="cursor-pointer md:text-lg md:font-medium ">
