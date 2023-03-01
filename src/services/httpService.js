76641f08a5dc8018c1260210fdc8be9aa5a1b09d
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-03-01 23:40:03 +0330

    expand logic of code

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,2 +1,11 @@
 axios.defaults.baseURL = "http://localhost:5000/api";
+import axios from "axios";
 defaults.baseURL = "https://api.openweathermap.org/data/3.0";
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  delete: axios.delete,
+  put: axios.put,
+};
+export default http;
