4ba44d23828d7a148a192d8648df5d1ac0ca8ea9
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-03-03 02:00:27 +0330

    expand code

--- src/services/getWeatherDataServices.js
+++ src/services/getWeatherDataServices.js
@@ -1,5 +1,15 @@
-import http from "./httpService";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-const getWeatherData = () => {};
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({
+    ...searchParams,
+    appid: import.meta.env.VITE_API_KEY,
+  });
+  console.log(url);
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
 
 export default getWeatherData;
