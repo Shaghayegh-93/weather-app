bbe2fa68b076bb74af1fb3accaeb00010f9923f3
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-03-03 01:58:39 +0330

    add getData function

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import DetailWeatherInfo from "./components/DetailWeatherInfo";
 import InputCity from "./components/InputCity";
 import TimeAndLocation from "./components/TimeAndLocation";
 import WeatherForecast from "./components/WeatherForecast";
+import getWeatherData from "./services/getWeatherDataServices";
 
 function App() {
+  const getData = async () => {
+    const data = await getWeatherData("weather", { q: "London" });
+  };
+
+  getData();
+
   return (
-    
-      <div className=" max-w-screen-md h-fit mx-auto bg-green-200 my-4 py-4 sm:px-32 px-4 shadow-xl ">
-        <CityList />
-        <InputCity />
-        <TimeAndLocation />
-        <DetailWeatherInfo />
-        <WeatherForecast title="HOURLY FORECAT" />
-        <WeatherForecast title="DAILY FORECAT" />
-      </div>
-  
+    <div className=" max-w-screen-md h-fit mx-auto bg-green-200 my-4 py-4 sm:px-32 px-4 shadow-xl ">
+      <CityList />
+      <InputCity />
+      <TimeAndLocation />
+      <DetailWeatherInfo />
+      <WeatherForecast title="HOURLY FORECAT" />
+      <WeatherForecast title="DAILY FORECAT" />
+    </div>
   );
 }
 
