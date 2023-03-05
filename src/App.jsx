8f76046424375bf3046a28e354de8a2dc6991501
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-03-05 16:44:47 +0330

    expand logic of code based on project

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import DetailWeatherInfo from "./components/DetailWeatherInfo";
 import InputCity from "./components/InputCity";
 import TimeAndLocation from "./components/TimeAndLocation";
 import WeatherForecast from "./components/WeatherForecast";
-import getWeatherData from "./services/getWeatherDataServices";
+import getFormatedWeatherData from "./services/getWeatherDataServices";
 
 function App() {
   const getData = async () => {
-    const data = await getWeatherData("weather", { q: "London" });
+    const data = await getFormatedWeatherData({ q: "London" });
+    console.log(data)
   };
 
   getData();
