599210482371dec3d1644c5d983bf4a40b646af6
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-03-07 03:16:06 +0330

    remove base url

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,5 @@
-axios.defaults.baseURL = "http://localhost:5000/api";
 import axios from "axios";
-defaults.baseURL = "https://api.openweathermap.org/data/3.0";
+// BASE_URL = "https://api.openweathermap.org/data/3.0";
 
 const http = {
   get: axios.get,
