e8d532035263b60fedf8e4d9875eca4e3f884932
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-03-09 23:22:39 +0330

    try to find and remove bug

--- src/services/getWeatherDataServices.js
+++ src/services/getWeatherDataServices.js
@@ -1,3 +1,5 @@
+import { DateTime } from "luxon";
+
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
@@ -6,10 +8,83 @@ const getWeatherData = (infoType, searchParams) => {
     ...searchParams,
     appid: import.meta.env.VITE_API_KEY,
   });
-  console.log(url);
-  return fetch(url)
-    .then((res) => res.json())
-    .then((data) => data);
+  return fetch(url).then((res) => res.json());
 };
 
-export default getWeatherData;
+const formatCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    sys: { country, sunrise, sunset },
+    wind: { speed },
+    weather,
+    dt,
+    name,
+  } = data;
+  const { main: details, icon } = weather[0];
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    country,
+    sunrise,
+    sunset,
+    speed,
+    details,
+    icon,
+    dt,
+    name,
+  };
+};
+const formatForecastWeather = (data) => {
+  const { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "ccc"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
+  hourly = hourly.slice(1, 6).map((d) => {
+    return {
+      title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
+  return { daily, hourly, timezone };
+};
+
+const getFormatedWeatherData = async (searchParams) => {
+  const formattedCurrentWeather = await getWeatherData(
+    "weather",
+    searchParams
+  ).then(formatCurrentWeather);
+
+  const { lat, lon } = formattedCurrentWeather;
+  console.log(lat, lon);
+
+  const formattedForecastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: searchParams.units,
+  }).then(formatForecastWeather);
+
+  return { ...formattedCurrentWeather, ...formattedForecastWeather };
+};
+
+const formatToLocalTime = (
+  secs,
+  zone,
+  format = "cccc,dd LLL yyy' |Local time: 'hh:mm a"
+) => {
+  DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+};
+
+export default getFormatedWeatherData;
